618671d88d5065cc4c2e67bf8b399b287a3c7c03
Joey <joey.felip11@gmail.com>
2021-06-25 10:43:54 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { AuthContextProvider } from './contexts/AuthContext'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/adminRoom";
+
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" exact component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
