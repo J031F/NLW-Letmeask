5fddc5507dd3547f713a2ad3c2b627fcf78d7b15
Joey Felipe <joey.felip11@gmail.com>
2021-06-26 20:00:52 -0500

    commit

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,7 +8,6 @@ import answerImg from '../assets/images/answer.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
@@ -19,19 +18,20 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-  // const { user } = useAuth();
   const history = useHistory()
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId)
-
+  
   async function handleEndRoom() {
-    await database.ref(`rooms/${roomId}`).update({
-      endedAt: new Date(),
-    })
+    if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      await database.ref(`rooms/${roomId}`).update({
+        endedAt: new Date(),
+      })
 
-    history.push('/');
+      history.push('/');
+    }
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -101,7 +101,7 @@ export function AdminRoom() {
                   onClick={() => handleDeleteQuestion(question.id)}
                 >
                   <img src={deleteImg} alt="Remover pergunta" />
-                </button>
+                </button>                
               </Question>
             );
           })}
